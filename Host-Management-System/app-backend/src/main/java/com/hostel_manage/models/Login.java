ebefadba6cbba10de56bc32fecbcf210c41e5d80
reyanson <79089809+reyanson@users.noreply.github.com>
2023-10-01 12:13:46 +0530

    update back-end

---
+++ Host-Management-System/app-backend/src/main/java/com/hostel_manage/models/Login.java
@@ -0,0 +1,23 @@
+package com.hostel_manage.models;
+
+public class Login {
+    private String email;
+    private String password;
+    private String Test;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
