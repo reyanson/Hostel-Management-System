1f9323cc27d7b875054827e599c5d9b619f2e060
Darsi2122 <85359812+Darsi2122@users.noreply.github.com>
2023-10-25 16:14:11 +0530

    Update Registration.js

--- Host-Management-System/react-frontend/src/Registration.js
+++ Host-Management-System/react-frontend/src/Registration.js
@@ -52,7 +52,7 @@ export default function Registration(){
                 />
               </div>
               <div class="form-group">
-                <label>Last Name</label>
+                <label>Personal Number</label>
                 <input type="text" class="form-control" id="personalnum" 
                 placeholder="Enter personalnum" value={personalnum}
                 onChange={(event)=> {setPersonalnum(event.target.value);
@@ -60,7 +60,7 @@ export default function Registration(){
                 />
               </div>
               <div class="form-group">
-                <label>Last Name</label>
+                <label>Office Number</label>
                 <input type="text" class="form-control" id="officenum" 
                 placeholder="Enter officenum" value={officenum}
                 onChange={(event)=> {setOfficenum(event.target.value);
@@ -70,99 +70,6 @@ export default function Registration(){
             </form>
           </div>
           </div>
-
-          {/* <form onSubmit={(e) => onSubmit(e)}>
-            <div className="mb-3">
-              <label htmlFor="Userid" className="form-label">
-                User Id
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder="Enter team leader id"
-                name="u_id"
-                //value={u_id}
-                required
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Mtitle" className="form-label">
-                Meeting Title/Type
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder="Enter meeting title"
-                name="meeting_title"
-                //value={meeting_title}
-                required
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Aid" className="form-label">
-                Attendees Id
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder="Enter attendees id"
-                name="attendees_id"
-                value={attendees_id}
-                required
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Status" className="form-label">
-                Status
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder="Enter attendees status"
-                name="attendance_status"
-                value={attendance_status}
-                required
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Date" className="form-label">
-                Meeting Date
-              </label>
-              <input
-                type={"date"}
-                className="form-control"
-                placeholder="Select the date"
-                name="meeting_date"
-                value={meeting_date}
-                required
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="Remark" className="form-label">
-                Remark
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder="Enter the remark"
-                name="remark"
-                value={remark}
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <button type="submit" className="btn btn-outline-primary">
-              Marking
-            </button>
-            <Link className="btn btn-outline-danger mx-2" to="/">
-              Cancel
-            </Link>
-          </form> */}
-
         </div>
     );
 }
