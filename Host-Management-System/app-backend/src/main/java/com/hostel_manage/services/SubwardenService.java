37c4fc3cbc3d2ca423a41250dcd8ff1d302e00a8
reyanson <79089809+reyanson@users.noreply.github.com>
2023-11-05 22:23:50 +0530

    backend update

--- Host-Management-System/app-backend/src/main/java/com/hostel_manage/services/SubwardenService.java
+++ Host-Management-System/app-backend/src/main/java/com/hostel_manage/services/SubwardenService.java
@@ -1,8 +1,28 @@
 package com.hostel_manage.services;
 
 
+import com.hostel_manage.repository.SubwardenRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubwardenService {
+
+    @Autowired
+    SubwardenRepository subwardenRepository;
+
+    //Declare the Entity Manager
+    private final EntityManager entityManager;
+
+
+    public SubwardenService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    //method for complain action and accept change
+    public String updateSubwardenDaetails(int swardenId,String new_first_name,String new_last_name,String new_email,String new_nic,String new_address,int new_personal_no,int new_office_no) {
+        return subwardenRepository.updateSubwardenDaetails(swardenId,new_first_name,new_last_name,new_email,new_nic,new_address,new_personal_no,new_office_no);
+    }
 }
