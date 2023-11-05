dc4d42192590ad65faa3098e6a5c5d412050b077
Darsi2122 <85359812+Darsi2122@users.noreply.github.com>
2023-11-05 10:23:08 +0530

    room_asset_backend

--- Host-Management-System/react-frontend/src/scenes/assets/index.jsx
+++ Host-Management-System/react-frontend/src/scenes/assets/index.jsx
@@ -1,37 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Header from '../../Components/Header';
 import axios from 'axios';
 import Topbar from '../global/Topbar';
 import Sidebar from '../global/Sidebar';
 
-export default function ComplainTable() {
-
-
-    const [complaints, setComplaints] = useState([]);
+export default function RoomAssetsTable() {
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        loadComplaints();
-    }, []);
-
-    const loadComplaints = async () => {
-        try {
-            const response = await axios.get("http://localhost:8080/complains/viewall"); 
-            setComplaints(response.data);
-        } catch (error) {
-            console.error('Error fetching complaint data:', error);
-        }
-    };
-
-    const deleteComplaint = async (cId) => {
-        try {
-            await axios.delete(`http://localhost:8080/complains/delete/{complainId}`);
-            loadComplaints();
-        } catch (error) {
-            console.error('Error deleting complaint:', error);
-        }
-    };
+      loadAssets();
+  }, []);
+  
+  
+  const loadAssets = async () => {
+    try {
+        const response = await axios.get("http://localhost:8080/room-assets/get-all-asset-info");
+        setData(response.data);
+    } catch (error) {
+        console.error('Error fetching', error);
+    }
+  };
+  
 
     return (
         <>
@@ -48,57 +38,28 @@ export default function ComplainTable() {
 
                 <Box sx={{ display: 'flex' }}>            
                     <Box mt={-6} ml={4}>
-                        <div className='container mt-5'>
-                            <div className='py-4'>
-                                <table className="table border shadow">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Complaint ID</th>
-                                            <th scope="col">Registration Number</th>
-                                            <th scope="col">Category</th>
-                                            <th scope="col">Subject</th>
-                                            <th scope="col">Description</th>
-                                            <th scope="col">Created At</th>
-                                            <th scope="col">Updated At</th>
-                                            <th scope="col">Actions</th>
-                                            <th scope="col">Remark</th>
-                                            <th scope="col">Options</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {complaints.map((complaint, index) => (
-                                            <tr key={index}>
-                                                <td>{complaint.cId}</td>
-                                                <td>{complaint.regNo}</td>
-                                                <td>{complaint.category}</td>
-                                                <td>{complaint.subject}</td>
-                                                <td>{complaint.description}</td>
-                                                <td>{complaint.createdAt}</td>
-                                                <td>{complaint.updatedAt}</td>
-                                                <td>{complaint.action}</td>
-                                                <td>{complaint.remark}</td>
-
-                                                <td>
-                                                    <Link to={`/complaint/${complaint.cId}`} className="btn btn-info btn-sm mx-2">
-                                                        View
-                                                    </Link>
-                                                    <button
-                                                        onClick={() => {
-                                                            if (window.confirm('Are you sure you want to delete this complaint?')) {
-                                                                deleteComplaint(complaint.cId);
-                                                            }
-                                                        }}
-                                                        className="btn btn-danger btn-sm mx-2"
-                                                    >
-                                                        Delete
-                                                    </button>
-                                                </td>
+                    <div className='container mt-5'>
+                                <div className='py-4'>
+                                    <table className="table border shadow">
+                                        
+                                        {/* <table style={{ border: '1px solid black', borderCollapse: 'collapse' }}> */}
+                                          <thead>
+                                            <tr>
+                                              <th>Name</th>
+                                              <th>Room</th>
                                             </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                                          </thead>
+                                          <tbody>
+                                            {data.map(item => (
+                                              <tr key={item.id}>
+                                                <td>{item.name}</td>
+                                                <td>{item.room_no}</td>
+                                              </tr>
+                                            ))}
+                                          </tbody>
+                                          </table>
+                                </div>
+                            </div>                       
                     </Box>
                 </Box>
             </Box>
